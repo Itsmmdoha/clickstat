2e7de140dae9b9d65d99de78b9458d4a593a1445
itsmmdoha <getmonazirnow@gmail.com>
2023-08-29 20:32:53 +0600

    made a basic structure for testing the concept

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask,request,render_template,redirect
+from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     r = render_template("index.html")
     return r
-@app.route("/data",methods=["POST"])
+@app.route("/getlink",methods=["POST"])
 def data():
-    pass
+    data = request.json
+    print(data)
+    linkData = {"link" : "https://google.com"}
+    return linkData
 
 
 if __name__ == "__main__":
